57b56bb738d8374f2fb70e4a13db0e68d602d4ba
Chinmay Shet <chishxd@gmail.com>
2026-02-20 22:18:08 +0530

    add endpoint for login

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ def home():
 def add(num1, num2):
     num3 = int(num1) + int(num2)
     return str(num3)
+
 @app.route("/sub/<int:num1>/<int:num2>")
 def sub(num1, num2):
     num3 = int(num1) - int(num2)
     return str(num3)
 
+
+@app.route("/login/<username>/<password>")
+def login(username, password):
+    if username == "admin" and password == "1234":
+        return "Login successful"
+    return "Login failed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True) #localhost:8001
